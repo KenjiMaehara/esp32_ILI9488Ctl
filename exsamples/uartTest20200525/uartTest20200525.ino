3e5cf612847b4d4bc35e740550570f0a2aca04d6
techhouse_150908 <kmaehara@mel.co.jp>
2020-05-25 13:48:49 +0900

    Create uartTest20200525.ino

---
+++ exsamples/uartTest20200525/uartTest20200525.ino
@@ -0,0 +1,23 @@
+int n=0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial2.begin(115200);
+
+}
+
+void loop() {
+  n = n + 1;
+  Serial.print(n, DEC);  // ASCIIコードを十進数で出力
+  Serial.print(":hello_UART_0");
+  Serial.println("");   //改行
+
+  //Serial1.print("hello_UART_1");
+  //Serial1.println("");   //改行
+  
+  Serial2.print(n, DEC);  // ASCIIコードを十進数で出力
+  Serial2.print(":hello_UART_2");
+  Serial2.println("");   //改行
+  
+  delay(1000);
+}
