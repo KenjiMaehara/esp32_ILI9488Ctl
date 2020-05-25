16214cc5e7bc981a1762c936fddc7ba7d94277d9
techhouse_150908 <kmaehara@mel.co.jp>
2020-05-25 13:58:52 +0900

    UART1のポート変更で信号が出力できていることを確認。

--- exsamples/uartTest20200525/uartTest20200525.ino
+++ exsamples/uartTest20200525/uartTest20200525.ino
@@ -2,6 +2,8 @@ int n=0;
 
 void setup() {
   Serial.begin(115200);
+  //Serial1.begin(115200);
+  Serial1.begin(115200, SERIAL_8N1, 32, 33);
   Serial2.begin(115200);
 
 }
@@ -12,8 +14,9 @@ void loop() {
   Serial.print(":hello_UART_0");
   Serial.println("");   //改行
 
-  //Serial1.print("hello_UART_1");
-  //Serial1.println("");   //改行
+  Serial1.print(n, DEC);  // ASCIIコードを十進数で出力
+  Serial1.print("hello_UART_1");
+  Serial1.println("");   //改行
   
   Serial2.print(n, DEC);  // ASCIIコードを十進数で出力
   Serial2.print(":hello_UART_2");
