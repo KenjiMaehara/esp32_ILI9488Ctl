87f11fe40739f43df82a415367984a7ac9fe8c0d
techhouse_150908 <kmaehara@mel.co.jp>
2020-05-25 18:42:13 +0900

    Update DFPlayerTest20200525.ino

--- exsamples/DFPlayerTest20200525/DFPlayerTest20200525.ino
+++ exsamples/DFPlayerTest20200525/DFPlayerTest20200525.ino
@@ -1,8 +1,23 @@
 HardwareSerial Serial_df(2);
+int number=0;
  
 void setup() {
     Serial_df.begin(9600);
 }
  
 void loop() {
+
+  Serial_df.write(0x7e);
+  Serial_df.write(0xff);
+  Serial_df.write(0x06);    //命令6バイト
+  Serial_df.write(0x12);    //指定フォルダ　　＊0x12 mp3フォルダ
+  Serial_df.write(0x00);
+  Serial_df.write(0x00);
+  Serial_df.write(number++);    //play
+  //Serial_df.write(0xfe);
+  //Serial_df.write(0xe8);
+  Serial_df.write(0xef);
+
+
+  delay(5000);
 }
