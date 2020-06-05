9c53c5c85930bfe19a1429dd274c7df85f5d4007
techhouse_150908 <kmaehara@mel.co.jp>
2020-06-05 18:32:07 +0900

    一部修正

--- exsamples/20200521_rtosTest/20200521_rtosTest.ino
+++ exsamples/20200521_rtosTest/20200521_rtosTest.ino
@@ -4,48 +4,26 @@
 
 #include <freertos/FreeRTOS.h>    /* FreeRTOSを用いるためのヘッダファイル */
 
-#define  LED1PIN   22    /* LED1のポート番号 */
-#define  LED2PIN   23    /* LED2のポート番号 */
+//#define  LED1PIN   22    /* LED1のポート番号 */
+//#define  LED2PIN   23    /* LED2のポート番号 */
 
-void  task1( void *param )
-{
-  int i = 0;
-  while( 1 ) {
-    i=i+1;
-    vTaskDelay(500);
-    Serial.print("task1 : ");
-    Serial.print(i,DEC);
-    Serial.println();
 
-  }
-}
 
-void  task2( void *param )
-{
-  int j = 0;
-  while( 1 ) {
-    j=j+1;
-    vTaskDelay(200);
-    Serial.print("task2 : ");
-    Serial.print(j,DEC);
-    Serial.println();
-  }
-}
 
 void setup()
 {
   Serial.begin(115200);
   Serial.print("setup()\n");
 
-  pinMode(LED1PIN, OUTPUT);
-  pinMode(LED2PIN, OUTPUT);
-  digitalWrite(LED1PIN, LOW);  /* LED1 off */
-  digitalWrite(LED2PIN, LOW);  /* LED2 off */
+  //pinMode(LED1PIN, OUTPUT);
+  //pinMode(LED2PIN, OUTPUT);
+  //digitalWrite(LED1PIN, LOW);  /* LED1 off */
+  //digitalWrite(LED2PIN, LOW);  /* LED2 off */
 
   /* create task */
   xTaskCreatePinnedToCore( task1,   /* タスクの入口となる関数名 */
                            "TASK1", /* タスクの名称 */
-                           0x800,   /* スタックサイズ */
+                           8192,   /* スタックサイズ */
                            NULL,    /* パラメータのポインタ */
                            1,       /* プライオリティ */
                            NULL,    /* ハンドル構造体のポインタ */
@@ -53,7 +31,7 @@ void setup()
 
   xTaskCreatePinnedToCore( task2,
                            "TASK2",
-                           0x800,
+                           8192,
                            NULL,
                            2,
                            NULL,
