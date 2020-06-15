0b044aa4fd3cbf9082eb3b36c90e45a0b6ebec80
techhouse_150908 <kmaehara@mel.co.jp>
2020-06-15 16:08:58 +0900

    wifiの設定コード変更
    https://lang-ship.com/blog/work/esp32-wi-fi-setting/
    こちらのwifi設定のスケッチの方法に変更。
    
    ESPtouchというスマホアプリから、ESP32のAPのSSID、パスワード登録ができるようになった。

--- exsamples/wifiManager/wifiManager.ino
+++ exsamples/wifiManager/wifiManager.ino
@@ -1,7 +1,23 @@
-#include <WiFi.h>
+//#include <WiFi.h>
+//#include <DNSServer.h>
+//#include <WebServer.h>
+//#include <WiFiManager.h>
+
+#if defined(ESP8266)
+#include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
+#else
+#include <WiFi.h>          //https://github.com/esp8266/Arduino
+#endif
+
+//needed for library
 #include <DNSServer.h>
+#if defined(ESP8266)
+#include <ESP8266WebServer.h>
+#else
 #include <WebServer.h>
-#include <WiFiManager.h>
+#endif
+#include <WiFiManager.h>         //https://github.com/tzapu/WiFiManager
+
 
 #define LED       2
 #define INTERVAL  1000
