699ae208e240d3714f39558bf1bc65b367bafa2f
techhouse_150908 <kmaehara@mel.co.jp>
2020-07-14 12:56:15 +0900

    Update testI2cMCP23017.ino

--- exsamples/testI2cMCP23017/testI2cMCP23017.ino
+++ exsamples/testI2cMCP23017/testI2cMCP23017.ino
@@ -1,20 +1,20 @@
 #include <Wire.h>
- 
+
 // デバイスアドレス(スレーブ)
 uint8_t DEVICE_ADDRESS = 0x20;  // 2進 100000
- 
+
 // 前回のボタンの状態
 // ※レジスタへの書き込み回数を減らす為です。
-int pre_button = LOW;  
- 
+int pre_button = LOW;
+
 void setup() {
 
   Serial.begin(115200);
   pinMode(27,INPUT);
-  
+
   // マスタとしてI2Cバスに接続する
-  Wire.begin();       
- 
+  Wire.begin(25,26);
+
   // 初期設定(IODIRA/IODIRB)
   Wire.beginTransmission(DEVICE_ADDRESS);
     // 先頭アドレスへ
@@ -22,60 +22,60 @@ void setup() {
     // I/O設定Aの全てを出力設定にする
     Wire.write(0x00);
     //// I/O設定Bの全てを出力設定にする
-    Wire.write(0x00);    
+    Wire.write(0x00);
   Wire.endTransmission();
 
 
- 
+
   // GPIOA(GPA0～GPA7)
   Wire.beginTransmission(DEVICE_ADDRESS);
     Wire.write(0x12);
-    // 全てのGPAピンをHIGH(出力)にする    
+    // 全てのGPAピンをHIGH(出力)にする
     Wire.write(0xff);
   Wire.endTransmission();
- 
+
   // GPIOB(GPB0～GPB7)
   Wire.beginTransmission(DEVICE_ADDRESS);
     Wire.write(0x13);
     //  GPB0をLOW(入力)でそれ以外のGPBピンはHIGH(出力)にする
     Wire.write(0xff); // 2進 11111110
-  Wire.endTransmission();   
+  Wire.endTransmission();
 }
- 
-void loop() {  
+
+void loop() {
   if (digitalRead(27) == HIGH){
     if(pre_button != HIGH){
       // 全てのGPBピンをHIGH(出力)にする
       Wire.beginTransmission(DEVICE_ADDRESS);
-        Wire.write(0x13);      
+        Wire.write(0x13);
         Wire.write(0xff); // 2進 11111111
       Wire.endTransmission();
-      
+
       Wire.beginTransmission(DEVICE_ADDRESS);
-        Wire.write(0x12);      
+        Wire.write(0x12);
         Wire.write(0xff); // 2進 11111111
       Wire.endTransmission();
-      
+
       pre_button = HIGH;
       delay(100);
       Serial.println("IO27 HIGH");
-    }    
+    }
   }else{
-    if(pre_button != LOW){    
+    if(pre_button != LOW){
       // GPB0をLOW(入力)でそれ以外のGPBピンはHIGH(出力)にする
       Wire.beginTransmission(DEVICE_ADDRESS);
-        Wire.write(0x13);      
+        Wire.write(0x13);
         Wire.write(0x00); // 2進 11111110
       Wire.endTransmission();
 
       Wire.beginTransmission(DEVICE_ADDRESS);
-        Wire.write(0x12);      
+        Wire.write(0x12);
         Wire.write(0x00); // 2進 11111110
       Wire.endTransmission();
-      
+
       pre_button = LOW;
       delay(100);
       Serial.println("IO27 LOW");
     }
   }
-}  
+}
