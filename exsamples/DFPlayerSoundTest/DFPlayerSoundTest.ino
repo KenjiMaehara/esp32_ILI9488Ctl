b78ec43c9f4734db62aa400137204023ed67ca4f
techhouse_150908 <kmaehara@mel.co.jp>
2020-07-17 19:48:56 +0900

    Update DFPlayerSoundTest.ino
    I2Cアドレス修正

--- exsamples/DFPlayerSoundTest/DFPlayerSoundTest.ino
+++ exsamples/DFPlayerSoundTest/DFPlayerSoundTest.ino
@@ -1,7 +1,7 @@
 #include <Wire.h>
 #include "Adafruit_MCP23017.h"
 
-Adafruit_MCP23017 mcp2;
+Adafruit_MCP23017 mcp1;
 
 HardwareSerial Serial_df(2);
 int number=0;
@@ -10,14 +10,14 @@ void setup() {
     Serial_df.begin(9600, SERIAL_8N1, 32, 33);
 
     Wire.begin(25,26);
-    mcp2.begin(0x01);
-    mcp2.pinMode(4, INPUT);
+    mcp1.begin(0x00);
+    mcp1.pinMode(4, INPUT);
 }
 
 void loop() {
 
 
-  if (mcp2.digitalRead(4)!=false) {
+  if (mcp1.digitalRead(4)!=false) {
       Serial_df.write(0x7e);
       Serial_df.write(0xff);
       Serial_df.write(0x06);    //命令6バイト
