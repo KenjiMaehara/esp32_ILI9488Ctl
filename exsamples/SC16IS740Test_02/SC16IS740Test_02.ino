d37d6c442d9185029717171840961e293b3d000a
techhouse_150908 <kmaehara@mel.co.jp>
2020-07-20 19:01:39 +0900

    Update SC16IS740Test_02.ino

--- exsamples/SC16IS740Test_02/SC16IS740Test_02.ino
+++ exsamples/SC16IS740Test_02/SC16IS740Test_02.ino
@@ -8,34 +8,43 @@
 #include <SPI.h>
 
 SC16IS750 i2cuart = SC16IS750(SC16IS750_PROTOCOL_I2C,SC16IS750_ADDRESS_AA);
+SC16IS750 i2cuart02 = SC16IS750(SC16IS750_PROTOCOL_I2C,SC16IS750_ADDRESS_AB);
 
 //Connect TX and RX with a wire and run this sketch
 
 #define baudrate 9600
 //#define baudrate 57600
 //#define baudrate 115200
-//#define baudrate 230400
+//#define baudrate 230400SS
 //#define baudrate 460800
 //#define baudrate 921600
 
 void setup()
 {
   Serial.begin(115200);
-  
-   //int SDA1=21   ;GPIO21を割当     2020/0528追加      
+
+   //int SDA1=21   ;GPIO21を割当     2020/0528追加
    //int SCL1=22   ;GPIO22を割当     2020/0528追加
-  Wire.begin(21,22);  // wake up I2C bus　     2020/0528追加  
+  Wire.begin(21,22);  // wake up I2C bus　     2020/0528追加
 
 
   Serial.println("start i2cuart");
   // UART to Serial Bridge Initialization
   i2cuart.begin(baudrate);               //baudrate setting
   if (i2cuart.ping()!=1) {
-      Serial.println("device not found");
+      Serial.println("i2cuart device not found");
       while(1);
   } else {
-      Serial.println("device found");
+      Serial.println("i2cuart device found");
   }
+
+  if (i2cuart02.ping()!=1) {
+      Serial.println("i2cuart02 device not found");
+      while(1);
+  } else {
+      Serial.println("i2cuart02 device found");
+  }
+
   Serial.print("start serial communication. baudrate = ");
   Serial.println(baudrate);
 }
@@ -49,19 +58,9 @@ void loop()
     // read the incoming byte:
     char c = i2cuart.read();
 
-#if 0
-    Serial.print("c=");
-    if (c < 0x20) {
-      Serial.print(" ");
-    } else {
-      Serial.print(c);
-    }
-    Serial.print(" 0x");
-    Serial.println(c,HEX);
-#endif
 
     if (c == 0x0d) {
-     
+
     } else if (c == 0x0a) {
       Serial.print("[");
       Serial.print(buffer);
