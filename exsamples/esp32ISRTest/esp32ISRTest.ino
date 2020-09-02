ee6989d89cf8fb358b6477fc7e7f5b1623d96a20
techhouse_150908 <kmaehara@mel.co.jp>
2020-09-02 10:10:43 +0900

    Update esp32ISRTest.ino

--- exsamples/esp32ISRTest/esp32ISRTest.ino
+++ exsamples/esp32ISRTest/esp32ISRTest.ino
@@ -5,10 +5,12 @@
  */
 
 //#define LED_Pin 27
-#define SW_Pin 33
+#define SW_Pin 35
 //volatile int state = LOW;
+volatile int gTest = 0;
 
 void setup() {
+  Serial.begin(115200);
   pinMode(SW_Pin, INPUT_PULLUP);
   //pinMode(LED_Pin, OUTPUT);
   attachInterrupt(SW_Pin, ISRTest, FALLING);
@@ -16,10 +18,14 @@ void setup() {
 
 void loop() {
   //digitalWrite(LED_Pin, state);
+    //Serial.println("test");
+    //delay(1000);
 }
 
 void ISRTest(){
-  delay(10);
+  gTest++;
   //state = !state;
-  Serial.println("ISR found");
+  Serial.print("ISR found:");
+  Serial.println(gTest);
+  delay(500);
 }
